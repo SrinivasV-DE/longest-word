64e88f3ef525a79710c60bffee65ec944e52e6f9
Srinivas Venkateswarlu <srini55de@gmail.com>
2024-10-03 17:19:40 +0000

    Deploy latest code

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,12 @@ from longest_word.game import Game
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    #return 'Hello World'
+    game = Game()
+    return render_template('home.html', grid=game.grid)
+
 @app.route('/check', methods=["POST"])
 def check():
     game = Game()
